90a2191c9b1d3d864c316cc6ddc2328c6c990df5
st-jliu <33519628+st-jliu@users.noreply.github.com>
2026-03-11 22:09:41 +0800

    Update nextjs config, build standalone output

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
+  output: "standalone",
+  turbopack: {},
   // next.js config
 });
