bcc2c3f2a375d6724bf35e642519901d83bf42ee
st-jliu <33519628+st-jliu@users.noreply.github.com>
2026-04-01 08:29:14 +0800

    Update service worker, avoid cache 401 basic auth, and tracking domain

--- sw/sw.ts
+++ sw/sw.ts
@@ -1,16 +1,32 @@
 /// <reference lib="webworker" />
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry } from "@serwist/precaching";
-import { installSerwist } from "@serwist/sw";
+import { installSerwist, type RuntimeCaching } from "@serwist/sw";
+import { NetworkOnly } from "serwist";
 
 declare const self: ServiceWorkerGlobalScope & {
   __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
 };
+
+const customCache: RuntimeCaching[] = [
+  {
+    matcher: ({ request }) => request.mode === 'navigate',
+    handler: new NetworkOnly(),
+  },
+  {
+    matcher: ({ url }) => {
+      const blockedDomains = ['px.ads.linkedin.com', 'www.googletagmanager.com', 'cmp.osano.com'];
+      return blockedDomains.some(domain => url.hostname.includes(domain));
+    },
+    handler: new NetworkOnly(),
+  },
+  ...defaultCache,
+];
 
 installSerwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
-  runtimeCaching: defaultCache,
+  runtimeCaching: customCache,
 });
