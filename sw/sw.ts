403f45b0448442b9443113632f5d85ceb605fef4
st-jliu <33519628+st-jliu@users.noreply.github.com>
2026-04-01 09:42:13 +0800

    Fix serwist cache external tracking url issue

--- sw/sw.ts
+++ sw/sw.ts
@@ -1,32 +1,16 @@
 /// <reference lib="webworker" />
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry } from "@serwist/precaching";
-import { installSerwist, type RuntimeCaching } from "@serwist/sw";
-import { NetworkOnly } from "serwist";
+import { installSerwist } from "@serwist/sw";
 
 declare const self: ServiceWorkerGlobalScope & {
   __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
 };
-
-const customCache: RuntimeCaching[] = [
-  {
-    matcher: ({ request }) => request.mode === 'navigate',
-    handler: new NetworkOnly(),
-  },
-  {
-    matcher: ({ url }) => {
-      const blockedDomains = ['px.ads.linkedin.com', 'www.googletagmanager.com', 'cmp.osano.com'];
-      return blockedDomains.some(domain => url.hostname.includes(domain));
-    },
-    handler: new NetworkOnly(),
-  },
-  ...defaultCache,
-];
 
 installSerwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
-  runtimeCaching: customCache,
+  runtimeCaching: defaultCache,
 });
